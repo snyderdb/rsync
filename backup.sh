75d814e89a58c043037faf43dd31fc78e0dbd8db
David Snyder <david_snyder85@yahoo.com>
2022-06-23 15:31:16 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 rsync -avzphic /home/dave/Videos/ dave@192.168.1.162:/home/dave/Videos/
+rsync -avzphic /home/dave/Pictures/ dave@192.168.1.162:/home/dave/Pictures/
+rsync -avzphic /home/dave/Documents/ dave@192.168.1.162:/home/dave/Documents/
 
