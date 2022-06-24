93448ee19ca6d7d4f4396caf7ef9b98f3ffd6a48
David Snyder <david_snyder85@yahoo.com>
2022-06-23 21:11:35 -0400

    Update backup.sh
    Added the --delete option.  This will delete any files in the target that are no longer found in source.

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync -avzphic /home/dave/Videos/ dave@192.168.1.162:/home/dave/Videos/
-rsync -avzphic /home/dave/Pictures/ dave@192.168.1.162:/home/dave/Pictures/
-rsync -avzphic /home/dave/Documents/ dave@192.168.1.162:/home/dave/Documents/
+rsync -avzphic --delete /home/dave/Videos/ dave@192.168.1.162:/home/dave/Videos/
+rsync -avzphic --delete /home/dave/Pictures/ dave@192.168.1.162:/home/dave/Pictures/
+rsync -avzphic --delete /home/dave/Documents/ dave@192.168.1.162:/home/dave/Documents/
  
